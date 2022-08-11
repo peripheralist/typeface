68c5a57a5b19f7835b8c4d1cfbb0ce9fbb44673a
peripheralist <peripheralist@gmail.com>
2022-08-11 15:26:56 -0500

    deploy script fix

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -6,12 +6,16 @@ import { ethers } from "hardhat";
 
 import { fonts } from "../fonts";
 import { reservedColors } from "../reservedColors";
-import { CapsulesToken, CapsulesTypeface } from "../typechain-types";
+import {
+  CapsulesMetadata,
+  CapsulesToken,
+  CapsulesTypeface,
+} from "../typechain-types";
 
 const network = process.env.HARDHAT_NETWORK;
 // const ownerAddress = "0x63A2368F4B509438ca90186cb1C15156713D5834";
 const ownerAddress = "0x817738DC393d682Ca5fBb268707b99F2aAe96baE";
-const feeReceiverAddress = "0x63A2368F4B509438ca90186cb1C15156713D5834";
+const feeReceiverAddress = "0x817738DC393d682Ca5fBb268707b99F2aAe96baE";
 
 const getDeployer = async () => (await ethers.getSigners())[0];
 
@@ -90,8 +94,40 @@ const deployCapsulesTypeface = async (
   return capsulesTypeface;
 };
 
+export async function deployCapsulesMetadata(capsulesTypefaceAddress: string) {
+  const deployer = await getDeployer();
+  console.log("Deploying CapsulesMetadata with the account:", deployer.address);
+
+  const CapsulesMetadata = await ethers.getContractFactory("CapsulesMetadata");
+
+  const args = [
+    capsulesTypefaceAddress
+  ]
+
+  const capsulesMetadata = (await CapsulesMetadata.deploy(
+    ...args
+  )) as CapsulesMetadata;
+
+  console.log(
+    chalk.green(` ✔ CapsulesMetadata deployed for network:`),
+    process.env.HARDHAT_NETWORK,
+    "\n",
+    chalk.magenta(capsulesMetadata.address),
+    `tx: ${capsulesMetadata.deployTransaction.hash}`
+  );
+
+  writeFiles(
+    "CapsulesMetadata",
+    capsulesMetadata.address,
+    args.map((a) => JSON.stringify(a))
+  );
+
+  return capsulesMetadata;
+}
+
 const deployCapsulesToken = async (
-  capsulesTypefaceAddress: string
+  capsulesTypefaceAddress: string,
+  capsulesMetadataAddress: string,
 ): Promise<CapsulesToken> => {
   const deployer = await getDeployer();
   console.log("Deploying CapsulesToken with the account:", deployer.address);
@@ -100,6 +136,7 @@ const deployCapsulesToken = async (
 
   const args = [
     capsulesTypefaceAddress,
+    capsulesMetadataAddress,
     feeReceiverAddress,
     reservedColors,
     royalty,
@@ -134,18 +171,24 @@ const deployCapsulesToken = async (
 
 const main = async () => {
   const deployer = await getDeployer();
-  const nonce = await deployer.getTransactionCount();
-  const nonceOffset = 1;
+  let nonce = await deployer.getTransactionCount();
   const expectedCapsulesTokenAddress = ethers.utils.getContractAddress({
     from: deployer.address,
-    nonce: nonce + nonceOffset,
+    nonce: nonce + 2,
   });
 
   const capsulesTypeface = await deployCapsulesTypeface(
     expectedCapsulesTokenAddress
   );
 
-  await deployCapsulesToken(capsulesTypeface.address);
+  const capsulesMetadata = await deployCapsulesMetadata(
+    capsulesTypeface.address
+  );
+
+  await deployCapsulesToken(
+    capsulesTypeface.address,
+    capsulesMetadata.address,
+  );
 
   console.log("Done");
 };
