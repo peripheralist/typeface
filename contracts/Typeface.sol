17f8ab6657c049a1334c1fccd2f1662a650ff52d
peripheralist <peripheralist@gmail.com>
2022-09-12 16:03:14 -0400

    -

--- contracts/Typeface.sol
+++ contracts/Typeface.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.8;
 
 import "@openzeppelin/contracts/utils/introspection/ERC165.sol";
 import "./interfaces/ITypeface.sol";
